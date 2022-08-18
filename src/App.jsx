ba99d62ce832f17c705df69875299f9676dc87ea
Aleff Soares <aleffsoares8@gmail.com>
2022-08-17 20:26:04 -0400

    Propriedades no componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Post from './Post';
-function App() {
+import { Post } from './Post';
+export function App() {
 
   return (
-      <Post />
+    <div>
+      <Post 
+        author="Aleff Soares"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum velit a iure! Officia, minus quasi asperiores maxime porro suscipit sit ex corporis doloribus. Nam perspiciatis, corrupti explicabo eaque assumenda soluta."
+      />
+
+      <Post 
+        author="Victor Soares"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </div>  
   )
 }
 
