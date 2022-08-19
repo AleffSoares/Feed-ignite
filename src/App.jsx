3fc56db59c703e516b41a41bf4ff20c1dfb1e540
Aleff Soares <aleffsoares8@gmail.com>
2022-08-18 20:57:15 -0400

    Criação dos estilos css e declaração de variaveis de cores

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
 
-import './styles.css';
+
+import './global.css';
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post 
         author="Aleff Soares"
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum velit a iure! Officia, minus quasi asperiores maxime porro suscipit sit ex corporis doloribus. Nam perspiciatis, corrupti explicabo eaque assumenda soluta."
       />
-
       <Post 
         author="Victor Soares"
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
       />
     </div>  
-  )
+  );
 }
 
 export default App
