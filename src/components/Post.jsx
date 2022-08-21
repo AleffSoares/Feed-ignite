c6b028d5c291f619ba90c5f8912e81a7a0a36504
Aleff Soares <aleffsoares8@gmail.com>
2022-08-21 16:04:36 -0400

    Estrutura HTML do componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+      <article className={styles.post}>
+          <header>
+            <div className={styles.author}>
+              <img className={styles.avatar} src="https://github.com/AleffSoares.png" alt="" />
+              <div className={styles.authorInfo}>
+                  <strong>Aleff Soares</strong>
+                  <span>Analista de Sistemas</span>
+              </div>
+            </div>
+
+            <time title='21 de Agosto às 14:56h' dataTime="2022-08-21 14:55:30">Publicado há 1h</time>
+          </header>
+
+          <div className={styles.content}>
+               <p>Fala galeraa 👋</p> 
+
+               <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no Ignite, evento da Rocketseat. O nome do projeto é Feed 🚀</p> 
+
+               <p>👉 <a href="">github.com/AleffSoares</a></p> 
+
+               <p><a href="">#novoprojeto #ignite #rocketseat</a></p> 
+          </div>
+      </article>
+  );
+}
